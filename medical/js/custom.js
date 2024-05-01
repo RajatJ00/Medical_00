6f725649002d70fd4a3db2735618f4c46f8a2fe1
Rajat Jamgade <rajatjamgade00@gmail.com>
2024-05-01 15:03:12 +0530

    Medical Project

---
+++ medical/js/custom.js
@@ -0,0 +1,15 @@
+// Get Current Year
+
+
+//client section owl carousel
+
+
+/** google_map js **/
+
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.712775, -74.005973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
